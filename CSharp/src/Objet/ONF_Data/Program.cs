aad791bcd1d6f54a794cf9d7b8b5924eb620ec15
kevin <kbourguignon@outlook.com>
2022-02-09 08:16:18 +0100

    update horse p2 suite

---
+++ CSharp/src/Objet/ONF_Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ONF_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
